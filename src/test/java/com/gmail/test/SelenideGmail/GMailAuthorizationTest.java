27f2108877c0165001cd9f73c5339aae029f7660
Eugene Chushkin <eugene.chushkin@gmail.com>
2016-03-12 12:03:09 +0200

    add testing.xml and parameters

--- src/test/java/com/gmail/test/SelenideGmail/GMailAuthorizationTest.java
+++ src/test/java/com/gmail/test/SelenideGmail/GMailAuthorizationTest.java
@@ -1,5 +1,6 @@
 package com.gmail.test.SelenideGmail;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import junit.framework.Assert;
@@ -11,20 +12,31 @@ import static com.codeborne.selenide.Condition.*;
 public class GMailAuthorizationTest {
 
 	@Test
-	public void search(){
+	@Parameters({"url"})
+	public void search(String url){
 	//	open("https://www.google.com.ua/");
 		//ScreenShooter.captureSuccessfulTests = true;
-		  GoogleSearchPage searchPage = open("https://www.google.com.ua/", GoogleSearchPage.class);
-		  GoogleResultsPage resultsPage = searchPage.search("selenide");
+		  GoogleSearchPage searchPage = open(url, GoogleSearchPage.class);
+		  GoogleResultsPage resultsPage = searchPage.search("java");
 		  resultsPage.results().shouldHave(size(10));
-		  resultsPage.results().get(0).shouldHave(text("Selenide: лаконичные и стабильные UI тесты на Java"));
+		  resultsPage.results().get(0).shouldHave(text("Javax"));
 	}
 	
 	@Test
-	public void login(){
+	public void loginOld(){
 		GoogleSearchPage searchPage = open("https://www.google.com.ua/", GoogleSearchPage.class);
 		GoogleLoginPage loginPage = searchPage.goToLoginPage();
 		GoogleBasePage basePage = loginPage.login("anry.chushkin@gmail.com", "128333333");
 		Assert.assertEquals("Андрей", basePage.getLoginName());
+	}
+	
+
+	@Test
+	@Parameters({"url","login", "password", "nickname"})
+	public void login(String url,String login, String password, String nickname){
+		GoogleSearchPage searchPage = open(url, GoogleSearchPage.class);
+		GoogleLoginPage loginPage = searchPage.goToLoginPage();
+		GoogleBasePage basePage = loginPage.login(login, password);
+		Assert.assertEquals(nickname, basePage.getLoginName());
 	}
 }
